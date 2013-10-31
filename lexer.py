787b684b832d5b5285ed2c3ff079722cfb15f9a3
soumya mandi <soumyamandi@gmail.com>
2013-10-31 13:48:49 +0530

    reserved words added.

--- lexer.py
+++ lexer.py
@@ -24,3 +24,49 @@ keywords = ( 'class',
         'not',
         'true', 
         )
+
+tokens = keywords + ( 'INTEGER',
+                    'PLUS',
+                    'MINUS',
+                    'TIMES',
+                    'DIVIDE',
+                    'LPAREN',
+                    'RPAREN',
+                    )
+
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Define a rule to track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print "Illegal character '%s'" % t.value[0]
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+
+#Test it out
+data = '''hello
+        I was here and
+        now I am gone.'''
+
+if __name__ == "__main__":
+
+    lexer.input(data)
+    # Tokenize
+    while True:
+	    tok = lexer.token()
+	    if not tok: break #No more input
+	    print tok.type, tok.value, tok.line, tok.lexpos
+

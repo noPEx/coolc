a5522bef5f7455732c566e88a7d939541884c5e9
soumya mandi <soumyamandi@gmail.com>
2013-10-31 13:32:13 +0530

    added keywords. first commit

---
+++ lexer.py
@@ -0,0 +1,26 @@
+# -----------------------------------------------
+# lexer.py
+# tokenizer for the cool language.
+import ply.lex as lex
+
+# keywords for cool
+keywords = ( 'class',
+        'else',
+        'false',
+        'fi',
+        'if',
+        'in',
+        'inherits',
+        'isvoid',
+        'let',
+        'loop',
+        'pool',
+        'then',
+        'while',
+        'case',
+        'esac',
+        'new',
+        'of',
+        'not',
+        'true', 
+        )
